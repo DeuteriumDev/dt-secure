2667b506b2f6167ae7cbe2a0e606b050de265594
Ben Vincent <benjc.vincent@gmail.com>
2025-07-14 14:32:21 -0600

    Update resource-permission modeling

--- access_manager_api/access_control/admin.py
+++ access_manager_api/access_control/admin.py
@@ -8,7 +8,9 @@ class ResourcePermissionAdmin(ModelAdmin):
 
 
 class UserResourcePermissionAdmin(ModelAdmin):
-    pass
+    # all fields on this model are derived from [ResourcePermissionAdmin]
+    def get_readonly_fields(self, request, obj):
+        return [a.attname for a in obj._meta.concrete_fields]
 
 
 admin.site.register(models.ResourcePermission, ResourcePermissionAdmin)
--- access_manager_api/access_control/models.py
+++ access_manager_api/access_control/models.py
@@ -2,6 +2,43 @@ import uuid
 from django.db import models
 
 
+class UserResourcePermission(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    user_id = models.CharField(null=False, blank=False)
+    resource_id = models.CharField(null=False, blank=False)
+
+    can_create = models.BooleanField(null=True)
+    can_read = models.BooleanField(null=True)
+    can_update = models.BooleanField(null=True)
+    can_delete = models.BooleanField(null=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user_id", "resource_id"]),
+        ]
+
+
+class ResourcePermissionManager(models.Manager):
+    def create(self, **kwargs):
+        instance = super(ResourcePermissionManager, self).create(
+            **kwargs,
+        )
+
+        UserResourcePermission(
+            permission=instance,
+            can_create=instance.can_create,
+            can_read=instance.can_read,
+            can_update=instance.can_update,
+            can_delete=instance.can_delete,
+        ).save()
+
+        return instance
+
+
 class ResourcePermission(models.Model):
     id = models.UUIDField(
         primary_key=True,
@@ -15,6 +52,7 @@ class ResourcePermission(models.Model):
         related_name="parent_permission",
         on_delete=models.SET_NULL,
     )
+    resource_id = models.CharField(null=False, blank=False)
     inherit_from_parent = models.BooleanField(default=True, null=False)
     created = models.DateTimeField(null=False, auto_now_add=True)
     updated = models.DateTimeField(null=False, auto_now=True)
@@ -23,8 +61,15 @@ class ResourcePermission(models.Model):
     can_read = models.BooleanField(null=True)
     can_update = models.BooleanField(null=True)
     can_delete = models.BooleanField(null=True)
+    user_resource = models.OneToOneField(
+        UserResourcePermission,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name="permission",
+    )
 
-    # group = relational field to group model
+    ## group = relational field to [CustomGroup](../accounts/models.py) model
+    objects = ResourcePermissionManager()
 
     class Meta:
         indexes = [
@@ -35,30 +80,3 @@ class ResourcePermission(models.Model):
         if self.inherit_from_parent and self.parent_permission:
             return f"{self.parent_permission.id}/{self.id} - {self.can_create}/{self.can_read}/{self.can_update}/{self.can_delete}"
         return f"{self.id} - {self.can_create}/{self.can_read}/{self.can_update}/{self.can_delete}"
-
-
-class UserResourcePermission(models.Model):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False,
-    )
-    user_id = models.CharField(null=False, blank=False)
-    resource_id = models.CharField(null=False, blank=False)
-    can_create = models.BooleanField(null=True)
-    can_read = models.BooleanField(null=True)
-    can_update = models.BooleanField(null=True)
-    can_delete = models.BooleanField(null=True)
-
-    permission = models.ForeignKey(
-        ResourcePermission,
-        null=False,
-        on_delete=models.PROTECT,
-        related_name="resources",
-    )
-
-    class Meta:
-        indexes = [
-            models.Index(fields=["user_id", "resource_id"]),
-            models.Index(fields=["permission"]),
-        ]
